a1721db9de568d20475e3e8a79ffe8a7456177bf
Pawan Singh <pawan.singh@leadschool.in>
2022-08-13 20:39:45 +0530

    Inorder Traversal of Binary Tree using Recursion

---
+++ src/BinaryTree_InorderRecursion.java
@@ -0,0 +1,38 @@
+public class BinaryTree_InorderRecursion {
+    private TreeNode root;
+    private class TreeNode{
+        private TreeNode left;
+        private  TreeNode right;
+        private int data;
+        public TreeNode (int data){
+            this.data=data;
+        }
+    }
+    public void createBinaryTree(){
+        TreeNode first=new TreeNode(1);
+        TreeNode second=new TreeNode(2);
+        TreeNode third=new TreeNode(3);
+        TreeNode fourth=new TreeNode(4);
+        TreeNode fifth=new TreeNode(5);
+        root=first;
+        first.left=second;
+        first.right=third;
+        second.left=fourth;
+        second.right=fifth;
+    }
+    public void preorder(TreeNode root)
+    {
+        if(root==null) {
+            return;
+        }
+        preorder(root.left);
+        System.out.println(root.data);
+        preorder(root.right);
+    }
+    public static void main(String[] args)
+    {
+        BinaryTree_InorderRecursion bt=new BinaryTree_InorderRecursion();
+        bt.createBinaryTree();
+        bt.preorder(bt.root);
+    }
+}
